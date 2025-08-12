82210294d8e1b43d3346ea5e3dcb8e4f8358b70b
Raine <raine.lin@baifu-tech.net>
2025-08-12 15:53:19 +0800

    chore: vite增加shader語言打包
    1.vite增加shader語言打包
    2.eslint no-unused-vars改成warning
    3.增加tsconfig及tsconfig.node.json

--- src/meshes/plane.ts
+++ src/meshes/plane.ts
@@ -1,10 +1,14 @@
 import * as THREE from "three";
+import { vertexShader, fragmentShader } from "../shaders/test";
 
 /**
  * Plane Mesh
  */
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
-const material = new THREE.MeshBasicMaterial();
+const material = new THREE.RawShaderMaterial({
+  vertexShader,
+  fragmentShader,
+});
 const planeMesh = new THREE.Mesh(geometry, material);
 
 export { planeMesh };
